1ae30284b7f7e2db6a2cc5a6414105e00a6a0635
Colin D Bennett <colin@gibibit.com>
2025-02-18 10:31:13 -0800

    feat: improved file read in chunks rather than entire file into RAM

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{path::PathBuf, time::Instant};
+//! Disk I/O benchmark test. Measure the read performance of filesystem access.
+//! Multithreaded with configurable concurrency.
+
+use std::{env, io::Read, os::unix::fs::MetadataExt, path::PathBuf, time::Instant};
 
 use clap::{Parser, Subcommand};
 use jwalk::{
@@ -8,7 +11,10 @@ use jwalk::{
     },
     DirEntry, WalkDir,
 };
+use tracing::{debug, trace};
+use tracing_subscriber::{layer::SubscriberExt as _, util::SubscriberInitExt as _, EnvFilter};
 
+/// Disk I/O benchmark performance test
 #[derive(Parser, Debug, Clone)]
 #[command(version, about, long_about = None)]
 struct Cli {
@@ -18,6 +24,7 @@ struct Cli {
 
 #[derive(Subcommand, Debug, Clone)]
 enum CliCommand {
+    /// Read a filesystem directory tree recursively.
     ReadTree {
         /// The directory to read instead of CWD.
         #[arg(short, long)]
@@ -29,6 +36,7 @@ enum CliCommand {
 }
 
 fn main() {
+    init_logging();
     let options = Cli::parse();
 
     match options.command {
@@ -107,16 +115,85 @@ impl ReadFilesStats {
 
 fn read_file(entry: &DirEntry<((), ())>) -> ReadFilesStats {
     let mut stats = ReadFilesStats::default();
-    let path = &entry.path();
-    match std::fs::read(path) {
-        Ok(content) => {
-            stats.bytes += content.len() as u64;
-            stats.file_count += 1;
+    let path = entry.path();
+
+    match do_read_file(&entry, &mut stats) {
+        Ok(()) => {
+            trace!("done reading file {}", path.to_string_lossy());
         }
         Err(err) => {
-            eprintln!("note: failed to read file {path:?}: {err}");
+            debug!("error reading file {}: {err}", path.to_string_lossy());
         }
     }
 
     stats
+}
+
+const BUF_SIZE: usize = 65536;
+
+fn do_read_file(
+    entry: &DirEntry<((), ())>,
+    stats: &mut ReadFilesStats,
+) -> Result<(), std::io::Error> {
+    let path = entry.path();
+    let pathstr = path.to_string_lossy();
+    trace!("open file: {}", pathstr);
+    let mut f = std::fs::File::open(&path)?;
+    stats.file_count += 1;
+    let size = entry.metadata()?.size();
+    let mb = size as f64 / 1e6;
+    trace!("begin reading file: {}", pathstr);
+    let mut buf = [0; BUF_SIZE];
+    loop {
+        let n = f.read(&mut buf[..])?;
+        if n == 0 {
+            if stats.bytes != size {
+                debug!("file must have been truncated, size was {size} but only read {} before getting empty read: {pathstr}", stats.bytes);
+            }
+            break;
+        }
+        stats.bytes += n as u64;
+        trace!(
+            "read chunk of {n} bytes ({:.1}% of {mb:.3} MB) from: {pathstr}",
+            100.0 * stats.bytes as f64 / size as f64,
+        );
+        if stats.bytes > size {
+            debug!("file must have been extended, size was {size} but we've read {}, stopping to avoid unbounded reading: {pathstr}", stats.bytes);
+            break;
+        }
+    }
+
+    Ok(())
+}
+
+const DEFAULT_LOGGING_DIRECTIVES: &str = "info,iobench=debug";
+
+fn init_logging() {
+    // Rust defaults to no backtraces on panic. Enable backtraces by default.
+    set_env_var_default("RUST_BACKTRACE", "1");
+
+    // Default logging level configuration if not set in the environment.
+    set_env_var_default("RUST_LOG", DEFAULT_LOGGING_DIRECTIVES);
+
+    tracing_subscriber::Registry::default()
+        // Use the RUST_LOG environment variable to configure logging levels.
+        // We disable 'ansi' so the color codes don't interfere with logs stored in files.
+        .with(EnvFilter::from_default_env())
+        .with(
+            tracing_subscriber::fmt::layer()
+                .with_ansi(false)
+                .with_thread_ids(true)
+                .with_thread_names(true),
+        )
+        .init();
+}
+
+fn set_env_var_default(name: &str, value: &str) {
+    if var_missing_or_blank(name) {
+        env::set_var(name, value);
+    }
+}
+
+fn var_missing_or_blank(name: &str) -> bool {
+    env::var(name).unwrap_or("".to_string()).trim().is_empty()
 }
